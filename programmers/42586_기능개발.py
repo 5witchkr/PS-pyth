8e9f4730303c71bf6dcf4c3075e9e50cd96d14db
5witchkr <95848796+5witchkr@users.noreply.github.com>
2022-05-30 21:27:22 +0900

    Update 42586_기능개발.py

--- programmers/42586_기능개발.py
+++ programmers/42586_기능개발.py
@@ -1,25 +1,3 @@
-##단순통과코드
-def solution(progresses, speeds):
-    stack1 = []
-    answer = []
-    count = 1
-    for i in progresses:
-        done = 100-i
-        k = -1
-        while done >= 0:
-            k += 1
-            done-=speeds[progresses.index(i)]
-        stack1.append(k)
 
-    for i in range(1,len(stack1)):
-        if stack1[i-1] >= stack1[i]:
-            count+=1
-        else:
-            answer.append(count)
-            count = 1
-    answer.append(count)
-    
-    return answer
-  
   ##큐스택 활용코드
   
