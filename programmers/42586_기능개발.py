a1dbdc450a2721c8d9dec75ee0f731c990c71767
5witchkr <95848796+5witchkr@users.noreply.github.com>
2022-05-30 22:06:04 +0900

    Update 42586_기능개발.py

--- programmers/42586_기능개발.py
+++ programmers/42586_기능개발.py
@@ -1,3 +1,25 @@
 
   ##큐스택 활용코드
   
+def solution(progresses, speeds):
+    list = []
+    queue = []
+    answer = []
+    for i in range(len(progresses)):
+        k = 1
+        while (100 > speeds[i] * k + progresses[i]):
+            k += 1
+        list.append(k)
+        
+    queue.append(list[0])
+        
+    for i in list[1:]:
+        if i > queue[0]:
+            answer.append(len(queue))
+            queue = []
+            queue.append(i)       
+        elif i <= queue[0]:
+            queue.append(i)
+    answer.append(len(queue))
+    
+    return answer
